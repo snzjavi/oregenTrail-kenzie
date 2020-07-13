c32903ce0e493ab86cda1f7fcf33f065878bb046
Javiere <snzjavi@gmail.com>
2020-07-13 09:02:14 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,55 @@
+class traveler {
+    constructor (name, food, isHeatlthy) {
+        this.name = name
+        this.food = food[1]
+        this.isHeatlthy === true
+    }
+    hunt () {
+        
+    }   
+    eat () {
+        
+        
+    }   
+}
+
+
+class wagon {
+    constructor (capacity, passengers) {
+        this.capacity = capacity
+        this.passengers = passengersArray()
+        
+    }
+    getAvailableSeatCount () {
+      
+    }   
+    join () {
+        
+    }   
+    shouldQuarantine () {
+
+    }
+
+    totalFood () {
+
+    }
+}
+
+
+// let wagon = new Wagon(2)
+// // Create three travelers
+// let henrietta = new Traveler('Henrietta')
+// let juan = new Traveler('Juan')
+// let maude = new Traveler('Maude')
+console.log(`Wagon Seat Count?: ${ wagon.getAvailableSeatCount() } – EXPECTED: 2. The wagon starts with 2 seats. We haven't added travelers to the wagon yet.`)
+// wagon.join(henrietta)
+console.log(`Wagon Seat Count?: ${ wagon.getAvailableSeatCount() } – EXPECTED: 1. Henrietta just joined.`)
+// wagon.join(juan)
+// wagon.join(maude)  // There is no room for her!
+// console.log(`Wagon Seat Count?: ${ wagon.getAvailableSeatCount() } – EXPECTED: 0 – There is no room for Maude, but Juan was able to join.`)
+// henrietta.hunt()   // Henrietta goes in search of food.
+// juan.eat()         // Juan eats – as Juan does. 🤣
+// juan.eat()         // Juan has run out of food!
+console.log(juan)
+console.log(`Wagon Should Quarantine?: ${ wagon.shouldQuarantine() } – EXPECTED: true. Juan has run out of food and become unhealthy!`)
+console.log(`Wagon's Total Food?: ${ wagon.totalFood() } – EXPECTED: 3.`)
